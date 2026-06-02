5835aa050503f200ba814a665fb3a562947d629c
Rahul Yadav <rahul.yadav06@nagarro.com>
2026-06-03 01:54:01 +0530

    build shared pacakge 3

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -8,23 +8,26 @@ import {
   OTLPTraceExporter
 } from '@opentelemetry/exporter-trace-otlp-http';
 
-const traceExporter =
-  new OTLPTraceExporter({
-    url: 'http://jaeger:4318/v1/traces'
+export const initTelemetry = () => {
+  const traceExporter =
+    new OTLPTraceExporter({
+      url: 'http://jaeger:4318/v1/traces'
+    });
+
+  const sdk = new NodeSDK({
+    traceExporter,
+    resource: resourceFromAttributes({
+      'service.name': process.env.SERVICE_NAME || 'unknown-service'
+    }),
+    instrumentations: [
+      getNodeAutoInstrumentations()
+    ]
   });
 
-const sdk = new NodeSDK({
-  traceExporter,
-  resource: resourceFromAttributes({
-    'service.name': process.env.SERVICE_NAME || 'unknown-service'
-  }),
-  instrumentations: [
-    getNodeAutoInstrumentations()
-  ]
-});
+  sdk.start();
 
-sdk.start();
+  console.log(
+    `Tracing initialized for ${process.env.SERVICE_NAME || 'unknown-service'}`
+  );
 
-console.log(
-  `Tracing initialized for ${process.env.SERVICE_NAME || 'unknown-service'}`
-);
+}
