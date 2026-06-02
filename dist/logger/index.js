5eee41e8aed2f1226bae7f065c145674e40c01d9
Rahul Yadav <rahul.yadav06@nagarro.com>
2026-06-03 03:47:58 +0530

    build shared pacakge 6

--- dist/logger/index.js
+++ dist/logger/index.js
@@ -5,6 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = void 0;
 const pino_1 = __importDefault(require("pino"));
+const api_1 = require("@opentelemetry/api");
 exports.logger = (0, pino_1.default)({
-    level: process.env.LOG_LEVEL || 'info'
+    level: process.env.LOG_LEVEL || 'info',
+    mixin() {
+        const span = api_1.trace.getActiveSpan();
+        if (!span) {
+            return {};
+        }
+        const ctx = span.spanContext();
+        return {
+            traceId: ctx.traceId,
+            spanId: ctx.spanId
+        };
+    }
 });
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,5 +1,21 @@
 import pino from 'pino';
+import { trace } from '@opentelemetry/api';
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info'
+  level: process.env.LOG_LEVEL || 'info',
+
+  mixin() {
+    const span = trace.getActiveSpan();
+
+    if (!span) {
+      return {};
+    }
+
+    const ctx = span.spanContext();
+
+    return {
+      traceId: ctx.traceId,
+      spanId: ctx.spanId
+    };
+  }
 });
